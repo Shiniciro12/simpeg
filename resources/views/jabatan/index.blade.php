424146e25d417fcd6f51b552b60dd90c8dcbacac
Kenny Wks <kenny.perulu@gmail.com>
2021-11-02 10:02:00 +0800

    fixing table jabatan

--- resources/views/jabatan/index.blade.php
+++ resources/views/jabatan/index.blade.php
@@ -26,13 +26,13 @@
         <table class="table table-hover table-striped shadow">
           <thead>
             <tr class="table-primary">
-              <th scope="col" class="text-center"></th>
               <th scope="col">#</th>
               <th scope="col">Jabatan</th>
               <th scope="col">Eselon</th>
               <th scope="col">Kelas</th>
               <th scope="col">Unit Kerja</th>
               <th scope="col">Jenis Jabatan</th>
+              <th scope="col" class="text-center">Aksi</th>
             </tr>
           </thead>
           <tbody>
@@ -46,19 +46,16 @@
               <td scope="row">{{ $row["nama_unit"] }}</td>
               <td scope="row">{{ $row["jenis_jabatan"] }}</td>
               <td scope="row" style="text-align: center;">
-                <a href="/jabatan/update/{{ $row['jabatan_id'] }}" class="btn btn-warning p-2 shadow"><i class="bi bi-pencil-square"></i></a>
+                <a href="/jabatan/update/{{ $row['jabatan_id'] }}" class="btn btn-warning p-2 shadow"><i
+                    class="bi bi-pencil-square"></i></a>
                 <form action="/jabatan/delete" class="d-inline" method="POST">
                   @csrf
                   <input type="hidden" value="{{$row['jabatan_id']}}" name="jabatan_id">
-                  <button class="btn btn-danger p-2 shadow" onclick="return confirm('Apakah anda ingin menghapus data ini?')"><i class="bi bi-trash-fill"></i></button>
+                  <button class="btn btn-danger p-2 shadow"
+                    onclick="return confirm('Apakah anda ingin menghapus data ini?')"><i
+                      class="bi bi-trash-fill"></i></button>
                 </form>
               </td>
-              <th scope="row">{{ $i++}}</th>
-              <td scope="row">{{ $row["nama_jabatan"] }}</td>
-              <td scope="row" class="text-center">{{ $row["eselon"] }}</td>
-              <td scope="row" class="text-center">{{ $row["kelas"] }}</td>
-              <td scope="row">{{ $row["nama_unit"] }}</td>
-              <td scope="row" class="text-center">{{ $row["jenis_jabatan"] }}</td>
             </tr>
             @endforeach
           </tbody>
