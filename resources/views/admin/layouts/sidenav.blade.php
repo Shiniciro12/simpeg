e6c9736cc9cf2fc5900e672f789536d43e57852c
Shiniciro12 <febykromen@gmail.com>
2021-11-15 10:48:10 +0700

    Front End Klien

---
+++ resources/views/admin/layouts/sidenav.blade.php
@@ -0,0 +1,46 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <div class="btn-md btn-outline-warning rounded-pill">
+            <a class="nav-link link-dark" aria-current="page" href="/klien">
+            <i class="bi bi-house-door-fill"></i>
+              Dashboard
+            </a>
+            </div>
+          </li>
+          <li class="nav-item">
+          <div class="btn-md btn-outline-warning rounded-pill">
+            <a class="nav-link link-dark" href="/klien/dataumum">
+            <i class="bi bi-grid-fill"></i>
+             Data Umum
+            </a>
+          </div>
+          </li>
+          <li class="nav-item">
+          <div class="btn-md btn-outline-warning rounded-pill">
+            <a class="nav-link link-dark" href="/klien/datakhusus">
+            <i class="bi bi-grid-fill"></i>
+              Data Khusus
+            </a>
+          </div>
+          </li>
+          
+        </ul>
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Rahmatilah</span>
+          </a>
+        </h6>
+        <ul class="nav flex-column mb-2">
+          <li class="nav-item">
+          <div class="btn-md btn-outline-warning rounded-pill">
+            <a class="nav-link link-dark" href="/">
+            <i class="bi bi-box-arrow-left"></i>
+              Logout
+            </a>
+          </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
