6d8b70973c1b0ce285e8474237aab69ceb90f616
Kenny Wks <kenny.perulu@gmail.com>
2021-11-21 17:53:35 +0800

    add migration, seeder, model for verifikasi

---
+++ app/Models/Verifikasi.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Verifikasi extends Model
+{
+    use HasFactory;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $primaryKey = 'verifikasi_id';
+    protected $table = 'verifikasi';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $guarded = ['unit_kerja_id'];
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            return $query->where('status', 'like', '%' . $search . '%');
+        });
+    }
+}
